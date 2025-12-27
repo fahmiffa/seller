73306662a91ac9a72cb374937050a49746920bfc
FFA <faisol.ajifa@gmail.com>
2025-12-27 10:19:37 +0700

    passowrd forget

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
 
 Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
